71b487ceee4ce3fee0cbf047c83156af726648a5
christinamcmahon <christina.mcmahon1@gmail.com>
2020-02-03 09:21:29 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(recordsArr) {
+  let result = recordsArr.find(rec => rec.result === "W")
+  return !!result ? result.year : undefined
+}
